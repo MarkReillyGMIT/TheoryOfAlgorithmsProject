78d35c633eb1090a7c8644b4aa0c1b86318bdd2b
MarkReillyGMIT <markreillyblue@hotmail.com>
2021-03-18 21:44:14 +0000

    Added Padding the Message for preprocessing

--- LabSHA256/preprocess.c
+++ LabSHA256/preprocess.c
@@ -10,7 +10,73 @@ union Block
 {
     BYTE bytes[64];
     WORD words[16];
+    uint64_t sixf[8];
 };
+
+enum Status{
+    READ, PAD0, PAD1, END
+};
+
+//Get the next block.
+int next_block(FILE *f, union Block *B, enum Status *S, uint64_t *nobits){
+    //Number of bytes read.
+    size_t nobytes;
+
+    if(*S == END){
+        return 0;
+    }else if (*S == READ){
+        //Try to read 64 bytes.
+        nobytes = fread(B->bytes, 1, 64, f);
+        //Calculate the total bits read so far.
+        *nobits += (8 * nobytes);
+        
+        if(nobytes ==64 ){
+            return 1;
+        }
+        // Enough room for padding.
+        else if(nobytes <= 55){
+            // Append a 1 bit (and seven 0 bits to make a full byte).
+            B->bytes[nobytes++] = 0x80; // In bits: 10000000
+            while (nobytes++ < 56)
+            {
+                B->bytes[nobytes] = 0x80; // In bits: 10000000
+            }
+            //Append length of orignal input
+            B->sixf[7]= *nobits;
+            //Say this is the last block
+            *S = END;
+        }
+        else{
+            //Gotten to the end of the input message.
+            //Not enough room in this block for all the padding.
+            //Append a 1 bit (and seven 0 bits to make a full byte)
+            B->bytes[nobytes] = 0x80;
+            //Append 0 bits.
+            while (nobytes++ < 64)
+            {
+                B->bytes[nobytes] = 0x80; // In bits: 10000000
+            }
+            // Change the status to PAD.
+            *S = PAD0;
+
+        }
+    }else if ((*S == PAD0))
+    {
+        nobytes = 0;
+        //Append 0 bits.
+        while (nobytes++ < 56)
+        {
+            B->bytes[nobytes] = 0x80; // In bits: 10000000
+        }
+        //Append 
+        B->sixf[7]=*nobits;
+        // Change the status to END.
+        *S = PAD0;
+    }
+    
+        
+    return 1;
+}
 
 
 int main(int argc, char *argv[]){
@@ -24,42 +90,22 @@ int main(int argc, char *argv[]){
     //Total number of bits read.
     uint64_t nobits = 0;
 
+    //Current status of reading input
+    enum Status S = READ;
+
     //File pointer for reading
     FILE*f;
     //Open file from command line for reading
     f = fopen(argv[1], "r");
 
-    //Number of bytes read.
-    size_t nobytes;
-    //Try to read bytes.
-    nobytes = fread(B.bytes, 1, 64, f);
-    //Tell the command line how many bytes we read.
-    printf("Read %ld bytes.\n", nobytes);
-    //Update of number of bits read.
-    nobits += (8 * nobytes);
-    //Print the 16 32-bit words.
-    for (i = 0; i < 16; i++)
+    //Loop through the (preprocessed) blocks from the input.
+    while (next_block(f, &B, &S, &nobits))
     {
-       printf("%08" PF " ", B.words[i]);
-       if (i + 1 % 8 == 0)
-       {
-           printf("\n");
-       }
-       
-    }
-    
-    while (!feof(f))
-    {
-
-        nobytes = fread(&B.bytes, 1, 64, f);
-        //Tell the command line how many bytes we read.
-        printf("Read %ld bytes.\n", nobytes);
-        nobits += (8 * nobytes);
+        //Print the 16 32-bit words
         for (i = 0; i < 16; i++){
-            printf("%08" PF " ", B.words[i]);
-            if (i + 1 % 8 == 0)
-                printf("\n"); 
-        }
+            printf("%08" PF " ", B.words[i]);     
+        }   
+        printf("\n");      
     }
     //Close file.
     fclose(f);
